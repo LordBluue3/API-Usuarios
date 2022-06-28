cd5417240c6e3cc47f4275d6e38a3684747aa359
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-06-28 17:58:59 -0300

    Recebendo conexão do Gerenciador de Usuários

--- src/main/java/com/mikael/ServerSockett.java
+++ src/main/java/com/mikael/ServerSockett.java
@@ -1,2 +1,34 @@
-package com.mikael;public class ServerSockett {
+package com.mikael;
+
+import com.mikael.connection.DataBaseConnector;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import java.util.List;
+
+public class ServerSockett {
+
+        public static int contador = 1;
+
+        public static void main(String[] args) throws IOException {
+
+            ServerSocket ss = new ServerSocket(5000);
+            List<String> listOfMessages = null;
+            Socket socket;
+
+
+            while(true) {
+                System.out.println("ServerSocket está conectando...");
+                System.out.println("Server conectado, aguardando requisições!!!");
+                DataBaseConnector db = new DataBaseConnector();
+                db.conectar();
+                socket = ss.accept(); //faz com que o while não continue a menos que uma mensagem seja requisitada
+                System.out.println("Conectado ao " + socket + "!");
+
+                socket.close();
+
+            }
+        }
 }
