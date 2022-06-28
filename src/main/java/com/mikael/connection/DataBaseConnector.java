9e17b47426f98569a589550df50b956beb604bb4
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-06-28 17:58:12 -0300

    Iniciando conexão com o banco de dados

--- src/main/java/com/mikael/connection/DataBaseConnector.java
+++ src/main/java/com/mikael/connection/DataBaseConnector.java
@@ -1,2 +1,26 @@
-package com.mikael.connection;public class DAO {
+package com.mikael.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConnector {
+    private String drive = "org.mariadb.jdbc.Driver";
+    private String url = "jdbc:mariadb://localhost:3306/dbusuarios";
+    private String usuario = "root";
+    private String senha = "";
+
+    Connection con =null;
+    public Connection conectar() {
+        try {
+            Class.forName(drive);
+            con = DriverManager.getConnection(url,usuario,senha);
+            System.out.println("connection on");
+            return con;
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
+
