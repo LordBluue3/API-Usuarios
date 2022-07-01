72243d9d005c1e448eba9ff3f39d06ea69d0b585
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-06-30 21:24:05 -0300

    Encapsulando atributos para configuração do connector

--- src/main/java/com/mikael/connection/Config.java
+++ src/main/java/com/mikael/connection/Config.java
@@ -1,2 +1,11 @@
-package com.mikael.connection;public class Config {
+package com.mikael.connection;
+
+import lombok.Getter;
+@Getter
+public class Config {
+    private String drive = "org.mariadb.jdbc.Driver";
+    private String url = "jdbc:mariadb://localhost:3306/dbusuarios";
+    private String usuario = "root";
+    private String senha = "";
+
 }
