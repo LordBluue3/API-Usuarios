12000e534b3e6c40354067e4ec39c56df50741de
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-07-04 20:23:28 -0300

    Método para desconectar do banco de dados

--- src/main/java/com/mikael/connection/DataBaseConnector.java
+++ src/main/java/com/mikael/connection/DataBaseConnector.java
@@ -12,10 +12,20 @@ public class DataBaseConnector {
             Connection con = DriverManager.getConnection(config.getUrl(),config.getUsuario(), config.getSenha());
             System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
             System.out.println("Conexão com o Banco de Dados realizado com exito");
+
             return con;
         } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
+    }
+    public Connection close() throws ClassNotFoundException, SQLException {
+
+            Class.forName(config.getDrive());
+            Connection con = DriverManager.getConnection(config.getUrl(),config.getUsuario(), config.getSenha());
+            con.close();
+            return con;
+
+
     }
 }
 
