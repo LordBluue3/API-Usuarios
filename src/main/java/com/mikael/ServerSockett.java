9dbbfe89729d34bda642d30ebf0a58f3e30acf95
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-07-04 20:22:19 -0300

    Chamando os métodos ler e imprimir mensagem

--- src/main/java/com/mikael/ServerSockett.java
+++ src/main/java/com/mikael/ServerSockett.java
@@ -1,6 +1,7 @@
 package com.mikael;
 
 import com.mikael.connection.DataBaseConnector;
+import com.mikael.message.Message;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,13 +9,14 @@ import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ServerSockett {
 
         public static void main(String[] args) throws IOException {
 
-            ServerSocket ss = new ServerSocket(5000);
+            ServerSocket ss = new ServerSocket(5000); //A porta que você colocou na sua aplicação
             List<String> listOfMessages = null;
             Socket socket;
 
@@ -26,6 +28,11 @@ public class ServerSockett {
                 socket = ss.accept(); //faz com que o while não continue a menos que uma mensagem seja requisitada
                 System.out.println("Conectado ao " + socket + "!");
                 db.conectar();
+                //ler mensagem do cliente
+                List listOfmessagen = new ArrayList<String>();
+                Message mensagem = new Message(socket, listOfMessages);
+                mensagem.lerMensagens();
+                mensagem.imprimirMensagem();
                 socket.close();
 
             }
