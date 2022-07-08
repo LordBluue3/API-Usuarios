4b27b453efb42bd813e9a516b840b7ca7c6d0136
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-07-08 14:53:34 -0300

    Encapsulando atributos

--- src/main/java/com/mikael/model/JavaBeans.java
+++ src/main/java/com/mikael/model/JavaBeans.java
@@ -1,2 +1,18 @@
-package com.mikael.model;public class JavaBeans {
+package com.mikael.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class JavaBeans {
+
+    private String idcon;
+    private String usuario;
+    private String email;
+    private String senha;
+
 }
