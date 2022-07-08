a2f32ad6ba05fe456dcd5f2f7bffb7dcde330e94
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-07-08 14:55:21 -0300

    Modificando nome dos objetos e adicionando comentários

--- src/main/java/com/mikael/connection/Actions.java
+++ src/main/java/com/mikael/connection/Actions.java
@@ -1,6 +1,7 @@
 package com.mikael.connection;
 
 import com.mikael.message.Message;
+import com.mikael.model.JavaBeans;
 import lombok.*;
 
 import java.net.Socket;
@@ -9,65 +10,64 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter
-@Setter
-@AllArgsConstructor
+
 public class Actions {
 
-    private String idcon;
-    private String usuario;
-    private String email;
-    private String senha;
 
+    /*
+     * Método que insere um usuário no banco de dados.
+     * Chama os atributos encapsulados na class JavaBeans.
+     *
+     * Chama o método da class DataBaseConnector conectar.
+     * Prepara a query para a execução no banco de dados.
+     * Substituir os parâmetros (?) pelo conteúdo das variaveis acima.
+     * Dando update na query
+     * Encerrando a conexão com o banco
+     */
+    @SneakyThrows
+    public void inserir(JavaBeans javaBeans){
+       String usuario = javaBeans.getUsuario();
+       String email = javaBeans.getEmail();
+       String senha = javaBeans.getSenha();
 
-    public void inserir(){
-        DataBaseConnector con = new DataBaseConnector();
-        String creat = "INSERT INTO usuarios (usuario,email,senha) values (?,?,?)";
+        DataBaseConnector dataBaseConnector = new DataBaseConnector();
+        String creatNewUser = "INSERT INTO usuarios (usuario,email,senha) values (?,?,?)";
 
-        try{
-            //Abrir a conexão
-           con.conectar();
-           //Preparando a query para execução do banco de dados
-            PreparedStatement pst = con.conectar().prepareStatement(creat);
-            //Substituir os parâmetros (?) pelo conteúdo das variaveis acima
-            pst.setString(1,usuario);
-            pst.setString(2,email);
-            pst.setString(3,senha);
-            //dando update na query
-            pst.executeUpdate();
-            //Encerrando a conexão com o banco
-            con.close();
+           dataBaseConnector.connection();
+            PreparedStatement preparedStatement = dataBaseConnector.connection().prepareStatement(creatNewUser);
+            preparedStatement.setString(1,usuario);
+            preparedStatement.setString(2,email);
+            preparedStatement.setString(3,senha);
+            preparedStatement.executeUpdate();
+            dataBaseConnector.close();
             System.out.println("Usuário inserido com sucesso");
             System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-
-        }catch (Exception e){
-
-        }
     }
-    public DataBaseConnector listarUsuarios(){
-        ArrayList<Actions> usuarios = new ArrayList<>();
+   /* public DataBaseConnector listarUsuarios(){
+        ArrayList<JavaBeans> usuarios = new ArrayList<>();
         Message msg = new Message(null, null);
         DataBaseConnector con = new DataBaseConnector();
         String read = "select * from usuarios order by usuario";
         try{
             con.conectar();
-            PreparedStatement pst = con.close().prepareStatement(read);
+            PreparedStatement pst = con.conectar().prepareStatement(read);
             ResultSet rs = pst.executeQuery();
             while(rs.next()){
                 String idcon = rs.getString(1);
                 String usuario = rs.getString(2);
                 String email = rs.getString(3);
                 String senha = rs.getString(4);
-                usuarios.add(new Actions(idcon,usuario,email,senha));
-                msg.enviarMensagem();
-
+                usuarios.add(new JavaBeans(idcon,usuario,email,senha));
+                msg.enviarUsuarios();
+                System.out.println("passou aqui");
             }
             con.close();
             return con;
         }catch (Exception f){
+            f.printStackTrace();
         }
         return con;
     }
-
+    */
 
 }
