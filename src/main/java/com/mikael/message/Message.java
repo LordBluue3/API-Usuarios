d64f8c8d896a185566c9523d2baa19a02b4a6eb1
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-07-08 14:54:17 -0300

    Removendo variaveis e adicionado o @SneakyThrowrs do lombok e adicionando comentários

--- src/main/java/com/mikael/message/Message.java
+++ src/main/java/com/mikael/message/Message.java
@@ -11,6 +11,8 @@ import java.net.Socket;
 import java.util.List;
 
 import com.mikael.connection.Actions;
+import com.mikael.model.JavaBeans;
+import lombok.SneakyThrows;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.json.simple.JSONObject;
@@ -19,10 +21,9 @@ import org.json.simple.JSONObject;
 
 public class Message {
 
-    public String idcon;
-    public String usuario;
-    public String email;
-    public String senha;
+
+
+    JavaBeans javaBeans = new JavaBeans(null,null,null,null);
 
     private final Socket socket;
     private List<String> listOfMessages = null;
@@ -32,61 +33,70 @@ public class Message {
         this.listOfMessages = listOfMessages;
 
     }
-    public void lerMensagens(){
-
-        try {
-            //obter o fluxo de entrada do soquete conectado
+    /*
+     * Método para ler as mensagens que foram enviadas da aplicação
+     *
+     * 0btem o fluxo de entrada do soquete conectado
+     * Cria um ObjectInputStream que possa ler os dados desse fluxo
+     * Lê as mensagens que estão armazenadas na variavel objectInputStream
+     */
+    @SneakyThrows
+    public void readMessage(){
             InputStream inputStream = socket.getInputStream();
-            //crie um DataInputStream para que possamos ler os dados dele.
             ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
-
-            // Leia a lista de mensagens do socket
-
-            try {
                 listOfMessages = (List<String>) objectInputStream.readObject();
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
             System.out.println("Revisto [" + listOfMessages.size() + "] mensagem ao: " + socket);
-
-
-        }catch (Exception e){
-            System.out.println(e);
-
-        }
     }
-
-    public void imprimirMensagem(){
-        Actions action = new Actions(idcon,usuario,email,senha);
+    /*
+    *  Método para imprimir as mensagens
+    *  foreach no ListOfMessages
+    *  Transformando essa String em Json
+    *  Transformando esse json em String e setando esses dados nas variaveis user, email e password
+    *  Chamando o método da class Action inserir
+    */
+    @SneakyThrows
+    public void PrintOutMessage(){
+        Actions action = new Actions();
         for (String msg : listOfMessages) {
 
-            try {
                 JSONObject parsed = (JSONObject) new JSONParser().parse(msg);
                 System.out.println("=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=");
                 System.out.println("Mensagem Recebida: ");
-                action.setUsuario((String) parsed.get("usuario"));
-                action.setEmail((String) parsed.get("email"));
-                action.setSenha((String) parsed.get("senha"));
-                System.out.println(action.getUsuario());
-                System.out.println(action.getEmail());
-                System.out.println(action.getSenha());
-                action.inserir();
-               action.listarUsuarios();
-
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
+                javaBeans.setUsuario((String) parsed.get("user"));
+                javaBeans.setEmail((String) parsed.get("email"));
+                javaBeans.setSenha((String) parsed.get("password"));
+                System.out.println(javaBeans.getUsuario());
+                System.out.println(javaBeans.getEmail());
+                System.out.println(javaBeans.getSenha());
+                action.inserir(javaBeans);
+                sendMessage();
         }
     }
-    public  void enviarMensagem(){
+    /*
+    * Método que envia mensagem a aplicação
+    */
+    @SneakyThrows
+    public  void sendMessage(){
+        DataOutputStream dataOutputStream = null;
+        Actions actions = new Actions();
+
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            dataOutputStream.writeUTF("Usuário cadastrado com sucesso");
+            System.out.println("enviando mensagem a aplicação");
+    }
+   /* public void enviarUsuarios(){
         DataOutputStream dos = null;
-        Actions actions = new Actions(idcon,usuario,email,senha);
+        Actions actions = new Actions();
         try{
             dos = new DataOutputStream(socket.getOutputStream());
-            dos.writeUTF(actions.getIdcon()+actions.getUsuario()+actions.getEmail()+actions.getSenha());
-            System.out.println("enviando mensagem");
-        }catch (Exception e){
+            dos.writeUTF("receba");
+            System.out.println("enviando usuário");
 
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
+    */
+
+
 }
