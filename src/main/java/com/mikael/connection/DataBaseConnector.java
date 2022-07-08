8fa8f273ef8ea5172aeafe687cbb54689348be84
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-07-08 14:54:38 -0300

    Adicionando @SneakyThrows

--- src/main/java/com/mikael/connection/DataBaseConnector.java
+++ src/main/java/com/mikael/connection/DataBaseConnector.java
@@ -1,31 +1,32 @@
 package com.mikael.connection;
 
+import lombok.SneakyThrows;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataBaseConnector {
     Config config = new Config();
-    public Connection conectar() {
-        try {
+    /*
+     * Método para se conectar ao banco de dados
+     */
+    @SneakyThrows
+    public Connection connection() {
             Class.forName(config.getDrive());
             Connection con = DriverManager.getConnection(config.getUrl(),config.getUsuario(), config.getSenha());
             System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
             System.out.println("Conexão com o Banco de Dados realizado com exito");
-
             return con;
-        } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
     }
+    /*
+     * Método para encerrar a conexão com o banco de dados
+     */
     public Connection close() throws ClassNotFoundException, SQLException {
-
             Class.forName(config.getDrive());
             Connection con = DriverManager.getConnection(config.getUrl(),config.getUsuario(), config.getSenha());
             con.close();
             return con;
-
-
     }
 }
 
